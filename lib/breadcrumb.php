f2cc3904a1a509a2a0341e4fb709f6da669b5234
Stephen Gehring <sgehringdesign@gmail.com>
2015-02-21 18:54:46 -0800

    add php front end modules

---
+++ lib/breadcrumb.php
@@ -0,0 +1,7 @@
+<ol class="breadcrumb">
+	<li><a href="/deploy/" ><strong>Deploy</strong></a></li>
+	<li class="active"><strong>Step 1:</strong> Name Deployment</li>
+	<li><a href="/deploy/deploy-2.php" ><strong>Step 2:</strong> Select Files</a></li>
+	<li><a href="/deploy/deploy-3.php" ><strong>Step 3:</strong> Confirm </a></li>
+	<li><a href="/deploy/deploy-4.php" ><strong>Step 4:</strong> Deploy </a></li>
+</ol>
