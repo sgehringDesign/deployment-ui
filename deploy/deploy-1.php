c11e772052c4f75a2801dfd0e837cf994bad2c86
Stephen Gehring <sgehringdesign@gmail.com>
2015-02-21 18:56:54 -0800

    add deployment front pages
    add deployment front pages for protoypeing the UI

---
+++ deploy/deploy-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Deploy 1: Name Deployment</title>
+  <?php include $_SERVER['DOCUMENT_ROOT']."/lib/head.php"; ?>
+</head>
+<body>
+	<div class="container-fluid">
+	  <?php include $_SERVER['DOCUMENT_ROOT']."/lib/header.php"; ?>
+	  <?php include $_SERVER['DOCUMENT_ROOT']."/lib/breadcrumb.php"; ?>
+	  <div>
+	  	[insert]	  
+	  </div>	
+	  <?php include $_SERVER['DOCUMENT_ROOT']."/lib/footer.php"; ?>
+</body>
+</html>
---
+++ deploy/deploy-3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Deploy 3: Confirm Deployment </title>
+  <?php include $_SERVER['DOCUMENT_ROOT']."/lib/head.php"; ?>
+</head>
+<body>
+	<div class="container-fluid">
+	  <?php include $_SERVER['DOCUMENT_ROOT']."/lib/header.php"; ?>
+	  <?php include $_SERVER['DOCUMENT_ROOT']."/lib/breadcrumb.php"; ?>
+	  <div>
+	  	[insert]	  
+	  </div>	
+	  <?php include $_SERVER['DOCUMENT_ROOT']."/lib/footer.php"; ?>
+</body>
+</html>
