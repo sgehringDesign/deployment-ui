c04ef17ead5da55c6ed9578d03ddc9c3bf7ecc97
Stephen Gehring <sgehringdesign@gmail.com>
2015-02-21 19:04:35 -0800

    traditional footer html

--- lib/footer.php
+++ lib/footer.php
@@ -1,3 +1,11 @@
 <footer id="footer-wrapper">
+	<!--
+	<ul>
+		<li><a href="#">link 1</a></li>
+		<li><a href="#">link 2</a></li>
+		<li><a href="#">link 3</a></li>
+	</ul>
 	
+	<p>copyright info</p>	
+	-->
 </footer>
