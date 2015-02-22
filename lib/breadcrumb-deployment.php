99899c9f98e14e06f35b8e95b78ccd6556350273
Stephen Gehring <sgehringdesign@gmail.com>
2015-02-21 23:37:02 -0800

    update bread crumb

--- lib/breadcrumb-deployment.php
+++ lib/breadcrumb-deployment.php
@@ -10,13 +10,12 @@
 		}		
 	}
 	
-?>
+?> 
 <div class="breadcrumb-wrapper">
 	<ol class="breadcrumb">
 		<li class="<?php echo getstate($pageName, 'index.php');    ?>" ><a href="/deploy/" ><strong>Deploy</strong></a></li>
 		<li class="<?php echo getstate($pageName, 'deploy-1.php'); ?>" ><a href="/deploy/deploy-1.php" ><strong>Step 1:</strong> Name Deployment</a></li>
 		<li class="<?php echo getstate($pageName, 'deploy-2.php'); ?>" ><a href="/deploy/deploy-2.php" ><strong>Step 2:</strong> Select Files</a></li>
-		<li class="<?php echo getstate($pageName, 'deploy-3.php'); ?>" ><a href="/deploy/deploy-3.php" ><strong>Step 3:</strong> Confirm </a></li>
-		<li class="<?php echo getstate($pageName, 'deploy-4.php'); ?>" ><a href="/deploy/deploy-4.php" ><strong>Step 4:</strong> Deploy </a></li>
+		<li class="<?php echo getstate($pageName, 'deploy-3.php'); ?>" ><a href="/deploy/deploy-3.php" ><strong>Step 4:</strong> Deploy </a></li>
 	</ol>
 </div>
