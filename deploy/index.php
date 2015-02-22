fdfc474088ef1543b5a67e82fa99b93a8d2d6a7c
Stephen Gehring <sgehringdesign@gmail.com>
2015-02-21 19:42:37 -0800

    adding index grid prototype html

--- deploy/index.php
+++ deploy/index.php
@@ -9,8 +9,35 @@
 	<div class="container-fluid">
 	  <?php include $_SERVER['DOCUMENT_ROOT']."/lib/header.php"; ?>
 	  <?php include $_SERVER['DOCUMENT_ROOT']."/lib/breadcrumb-deployment.php"; ?>
-	  <div>
-	  	[insert]	  
+	  <div class="screen-wrapper">  	
+	  	<div class="table-responsive grid-file-wrapper">
+		  	<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" >
+		  		<thead>
+						<tr>
+		  				<th class="text-center"><input type="checkbox" /></th>
+							<th>Deployment</th>
+							<th>Date</th>
+						</tr>
+		  		</thead>
+		  		<tbody>
+		  			<tr>
+		  				<td class="text-center"><input type="checkbox" /></td>
+		  				<td>Deployed Content Fix to Solutions Section</td>
+		  				<td>01/20/2015</td>
+						</tr>
+		  			<tr>
+		  				<td class="text-center"><input type="checkbox" /></td>
+		  				<td>Deployed Content Fix to Release 2015 Product Content</td>
+		  				<td>01/15/2015</td>
+						</tr>
+		  			<tr>
+		  				<td class="text-center"><input type="checkbox" /></td>
+		  				<td>Deployed Christmas Campaign</td>
+		  				<td>12/25/2014</td>
+						</tr>
+		  		</tbody>
+		  	</table>
+	  	</div>	
 	  </div>	
 	  <?php include $_SERVER['DOCUMENT_ROOT']."/lib/footer.php"; ?>
 </body>
