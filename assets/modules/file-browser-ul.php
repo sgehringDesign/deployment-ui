096ada45d79a362d8b042ad0c41d751d5f376c45
Stephen Gehring <sgehringdesign@gmail.com>
2015-03-07 14:55:23 -0800

    file browser include

---
+++ assets/modules/file-browser-ul.php
@@ -0,0 +1,186 @@
+<ul class="ul-file-browser">
+	<li>
+		<a href="#">
+			<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Products</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+		</a>
+		<ul>
+			<li>
+				<a href="#">
+					<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Product 1</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+				</a>
+				<ul>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Whats New</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Features</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Resources</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a href="#">
+					<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Product 2</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+				</a>
+				<ul>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Whats New</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Features</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Resources</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a href="#">
+					<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Product 3</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+				</a>
+				<ul>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Whats New</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Features</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Resources</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<a href="#">
+			<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Solutions</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+		</a>
+		<ul>
+			<li>
+				<a href="#">
+					<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Solution 1</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+				</a>
+				<ul>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Case Studies</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Resources</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a href="#">
+					<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Solution 2</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+				</a>
+				<ul>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Case Studies</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Resources</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a href="#">
+					<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Solution 3</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+				</a>
+				<ul>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Case Studies</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Resources</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<a href="#">
+			<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Customers</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+		</a>
+		<ul>
+			<li>
+				<a href="#">
+					<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Size</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+				</a>
+				<ul>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Small</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Medium</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Large</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+				</ul>
+			</li>
+			<li><a href="#"><span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Industry</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span></a>
+				<ul>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Healthcare</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Finance</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">...</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+						</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<a href="#">
+			<span class="span-caret-wrapper"><i class="caret right"></i></span><span class="span-file-name">Company</span><span class="span-select-children"><i class="glyphicon glyphicon-check"  data-toggle="tooltip" data-placement="left" title="Select all children"></i></span>
+		</a>
+	</li>
+</ul>
